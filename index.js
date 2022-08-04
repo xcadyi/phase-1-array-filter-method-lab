5b57eaa7ad4d617090736995543b49c6c58d0a86
Selin Ayyildiz <xcadyix@gmail.com>
2022-08-04 15:42:24 -0400

    commit tree beac17a6eb197a676d9618278d7c727d324efcf0

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+const { objectContaining } = require("expect");
+
 // Code your solution here
+function findMatching (drivers, string) {
+return drivers.filter(function (name) {    
+return name.toUpperCase() === string.toUpperCase()
+}) 
+}
+
+function fuzzyMatch (drivers, letter) {
+return drivers.filter((driver) => driver.startsWith(letter))
+}
+
+function matchName (drivers, string) {
+    console.log(drivers[string])
+}
